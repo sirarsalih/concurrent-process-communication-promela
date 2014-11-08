64aa281706ae4da724b83f95296b56dcde41237a
sirar87 <sirars@gmail.com>
2014-11-08 16:03:55 +0100

    Initial commit.

---
+++ concurrent_process_communication.c
@@ -0,0 +1,25 @@
+chan vw = [1] of {mtype};
+chan xy = [1] of {mtype};
+
+mtype = {a, b};
+
+proctype A()
+{
+	vw!a; 
+	xy!b;			
+}
+
+proctype B()
+{
+	xy?b;
+	vw?a;
+}
+
+init 
+{ 
+run A();
+run B();
+
+printf("a = %d\n", a);
+printf("b = %d\n", b);
+}
